7ce2c5c9175793f5fd9f3245a26ebb0e8f639f89
itamardag <itamar.dag@huji.ac.il>
2020-10-19 21:39:16 +0300

    initial commit
    modified beastify plugin. highlights clicked html element.
    Currently only title works, with a single selection.

---
+++ content_scripts/beastify.js
@@ -0,0 +1,60 @@
+(function () {
+    /**
+     * Check and set a global guard variable.
+     * If this content script is injected into the same page again,
+     * it will do nothing next time.
+     */
+    if (window.hasRun) {
+        return;
+    }
+    window.hasRun = true;
+
+    /**
+     * Given a URL to a beast image, remove all existing beasts, then
+     * create and style an IMG node pointing to
+     * that image, then insert the node into the document.
+     */
+    function insertBeast() {
+        document.addEventListener('click', function (e) {
+            //browser.runtime.sendMessage({
+            //    command: "saveElement",
+            //    content: e.target.className
+            //});
+            window.wrappedJSObject.prev = e.target;
+            e.target.style.backgroundColor = "#FF0000";
+        }, { once: true });
+    }
+
+    function setTargetId(id) {
+
+    }
+
+    /**
+     * Remove every beast from the page.
+     */
+    function removeExistingBeasts() {
+        let existingBeasts = document.querySelectorAll(".beastify-image");
+        for (let beast of existingBeasts) {
+            beast.remove();
+        }
+    }
+
+    /**
+     * Listen for messages from the background script.
+     * Call "beastify()" or "reset()".
+    */
+    browser.runtime.onMessage.addListener(handleMessage);
+
+    function handleMessage(message) {
+        if (message.command === "beastify") {
+            if (typeof (window.wrappedJSObject.prev) !== 'undefined')
+            {
+                window.wrappedJSObject.prev.style.backgroundColor = "initial";
+            }
+            insertBeast();
+        } else if (message.command === "reset") {
+            removeExistingBeasts();
+        }
+    }
+
+})();
